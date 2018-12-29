d586dad10d9f6b56f7014be56f6427cdf2481c05
Viswaz <viswaz@Viswazs-MacBook-Pro.local>
2018-12-28 20:20:48 -0600

    time taken to run, added

--- src/echo/echo_test.go
+++ src/echo/echo_test.go
@@ -1,26 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"testing"
+	"time"
 )
 
 func TestEchoOne(t *testing.T) {
+	start := time.Now()
 	s := []string{"", "This", "is", "to", "Test"}
 	if echoOne(s) != "This is to Test" {
 		t.Error("Expectation failed.")
 	}
+	fmt.Println("Time took in nano seconds.", time.Since(start).Nanoseconds())
 }
 
 func TestEchoTwo(t *testing.T) {
+	start := time.Now()
 	s := []string{"", "This", "is", "to", "Test"}
 	if echoTwo(s) != "This is to Test" {
 		t.Error("Expectation failed.")
 	}
+	fmt.Println("Time took in nano seconds.", time.Since(start).Nanoseconds())
 }
 
 func TestEchoThree(t *testing.T) {
+	start := time.Now()
 	s := []string{"", "This", "is", "to", "Test"}
 	if echoThree(s) != "This is to Test" {
 		t.Error("Expectation failed.")
 	}
+	fmt.Println("Time took in nano seconds.", time.Since(start).Nanoseconds())
 }
--- src/hello/hello_test.go
+++ src/hello/hello_test.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"testing"
+	"time"
 )
 
 func TestHello(t *testing.T) {
+	start := time.Now()
 	if hello() != "Hello World !!!" {
 		t.Error("Not expected value.")
 	}
+	fmt.Println("Time took in nano seconds.", time.Since(start).Nanoseconds())
 }
